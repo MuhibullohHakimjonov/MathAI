b9c7a5cc806d308ef071c2790c5af849a227402b
MuhibullohHakimjonov <mukhabillo@gmail.com>
2026-01-12 17:47:12 +0500

    Delete package directory

--- package/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-
-OLLAMA_API_KEY = os.getenv("OLLAMA_API_KEY")
-MINIMAX_URL = "https://ollama.com/api/chat"
-MINIMAX_MODEL = "gemini-3-flash-preview:cloud"
--- package/model.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class SolutionResponse(BaseModel):
-	solution: str
-
