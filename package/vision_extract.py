a70824b60e3767b9583c40458279339ea3cc79df
MuhibullohHakimjonov <mukhabillo@gmail.com>
2026-01-13 18:01:44 +0500

    Delete package directory

--- package/vision_extract.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-
-def extract_math_from_image(image_path: str) -> str:
-	with open(image_path, "rb") as f:
-		image_base64 = base64.b64encode(f.read()).decode()
-	return str(image_base64)
